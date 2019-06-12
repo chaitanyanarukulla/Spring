c273ff6f8d17e534d6daa965d02f98592800bae9
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-12 11:36:38 -0700

    initial commit

---
+++ src/main/java/com/chaiLab11/songr/Rcontroller.java
@@ -0,0 +1,17 @@
+package com.chaiLab11.songr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Rcontroller {
+    // specify the route (method and path) for this method
+        @GetMapping("/hello")
+        public String getHelloWorld() {
+            return "Hello, world! It's a beautiful day!";
+        }
+
+
+
+}
